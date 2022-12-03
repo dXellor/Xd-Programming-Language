f8eacd13c9a8edaa2c0bd8cea122304857381895
Nikola Simic <simic.ra32.2020@uns.ac.rs>
2022-12-03 14:28:26 +0100

    Updated header and scaner

--- xdheader.h
+++ xdheader.h
@@ -1,6 +1,20 @@
 #ifndef XDHEADER_H
 #define XDHEADER_H
 
-enum types {NO_TYPE, INT, FLOAT, CHAR, STRING, BOOL, VOID}
+#define CHAR_BUFFER_SIZE 128
+#define TRUE 1
+#define FALSE 0
+
+// Data types
+enum types {NO_TYPE, INT, FLOAT, CHAR, STRING, BOOL, VOID};
+
+// Arithmetic operators
+enum arops {ADD, SUB, MUL, DIV, MOD, INC, DEC};
+
+// Logical operators
+enum logop {LT, GT, LE, GE, EQ, NE};
+
+// Bitwise operators
+enum bitop {AND, OR, XOR, NOT};
 
 #endif
